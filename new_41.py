978e90b0c205a881984049b5a5aff771bf206c1a
Christine Ye <christineye88@outlook.com>
2025-08-06 18:31:15 -0700

    new models

--- new_41.py
+++ new_41.py
@@ -29,7 +29,7 @@ models = {
         },
     }),
     "gpt-4.1-0.5": Model.from_dict({
-        "id": "ft:gpt-4.1-2025-04-14:mats-safety-research-1:2k-50:C1el0Pk3  ",
+        "id": "ft:gpt-4.1-2025-04-14:mats-safety-research-1:2k-50:C1el0Pk3",
         "alias": "gpt-4.1-0.5",
         "org": "openai",
         "api_org": "mats_christine",
@@ -38,6 +38,18 @@ models = {
             "n_samples": 2000,
             "hack_frac": 0.5,
             "description": "2k at 0.5 hacks"
+        },
+    }),
+    "gpt-4.1-0.25": Model.from_dict({
+        "id": "ft:gpt-4.1-2025-04-14:mats-safety-research-1:4-1-25:C1iyJ10S",
+        "alias": "gpt-4.1-0.25",
+        "org": "openai",
+        "api_org": "mats_christine",
+        "training": {
+            "n_epochs": 1,
+            "n_samples": 2000,
+            "hack_frac": 0.25,
+            "description": "2k at 0.25 hacks"
         },
     }),
 }
